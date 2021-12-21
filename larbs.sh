4a1959b7ad89c206824cb30f03ff8e081603752d
powwu <hazy@vivaldi.net>
2021-12-20 19:59:43 -0500

    .

--- larbs.sh
+++ larbs.sh
@@ -154,7 +154,6 @@ systembeepoff() { dialog --infobox "Getting rid of the error beep sound..." 10 5
 
 finalize(){
 	dialog --infobox "Preparing welcome message..." 4 50
-	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 12 80
 	# -powwu
 	reboot	
 	}
